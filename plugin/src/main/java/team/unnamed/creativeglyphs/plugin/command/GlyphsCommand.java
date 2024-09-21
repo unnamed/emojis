754bc449536407155b682706735cb93913cb31cc
RiceChen_ <68499464+RICE0707@users.noreply.github.com>
2024-09-21 15:57:40 +0800

    Update GlyphsCommand.java

--- plugin/src/main/java/team/unnamed/creativeglyphs/plugin/command/GlyphsCommand.java
+++ plugin/src/main/java/team/unnamed/creativeglyphs/plugin/command/GlyphsCommand.java
@@ -211,7 +211,7 @@ public final class GlyphsCommand implements CommandClass {
 
         // send the header message
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', listConfig.getString("header", "Not found"))
-                .replace("<page>", String.valueOf(page + 1))
+                .replace("<page>", String.valueOf(_page + 1))
                 .replace("<maxpages>", String.valueOf(maxPages))
         );
 
@@ -220,7 +220,7 @@ public final class GlyphsCommand implements CommandClass {
 
         // send the footer message
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', listConfig.getString("footer", "Not found"))
-                .replace("<page>", String.valueOf(page + 1))
+                .replace("<page>", String.valueOf(_page + 1))
                 .replace("<maxpages>", String.valueOf(maxPages))
         );
     }
